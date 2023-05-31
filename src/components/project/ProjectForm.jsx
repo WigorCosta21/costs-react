8f8b84a42a61bd564bba737641e8886ff944a18a
wigor <wigor21@hotmail.com>
2023-05-30 22:32:45 -0300

    Connecting database.json API in project

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
+
 import Input from '../form/Input'
 import styles from './ProjectForm.module.css'
 import Select from './../form/Select';
 import SubmitButton from './../form/SubmitButton';
 
+
 const ProjectForm = ({ btnText }) => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const url = 'http://localhost:5000/categories'
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input type='text' text='Nome do projeto' name='name' placeholder='Insira o nome do projeto' />
       <Input type='number' text='Orçamento do projeto' name='buget' placeholder='Insira o orçamento total' />
-      <Select name='category_id' text='Selecione a categoria' />
+      <Select name='category_id' text='Selecione a categoria' options={categories} />
       <SubmitButton text={btnText} />
     </form>
   )
