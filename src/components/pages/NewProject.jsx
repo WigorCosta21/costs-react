7056e2d869916acb1dfd22f92373ccb0fa61c3cc
wigor <wigor21@hotmail.com>
2023-05-30 23:26:19 -0300

    Inserting data into the database via API

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,12 +1,38 @@
+import { useNavigate } from 'react-router'
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 const NewProject = () => {
+  const navegation = useNavigate()
+
+  const createPost = (project) => {
+    //initialize const and service
+    project.cost = 0
+    project.services = []
+
+    const url = 'http://localhost:5000/projects'
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+      .then((resp => resp.json()))
+      .then(data => {
+        // Redirect
+        navegation('/projects', { message: 'Projeto criado com sucesso!' })
+      })
+      .catch((err) => console.log(err))
+
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText='Criar Projeto' />
+      <ProjectForm handleSubmit={createPost} btnText='Criar Projeto' />
     </div>
   )
 }
